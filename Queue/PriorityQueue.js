aea47a6152312f1bc0d861331775a24f150b66fa
schacker <1039407128@qq.com>
2018-11-23 16:27:18 +0800

    优先队列

--- Queue/PriorityQueue.js
+++ Queue/PriorityQueue.js
@@ -1,29 +1,82 @@
-function PriorityQueue(){
-  let items = []
-  function QueueElement(el, pri) {
-    this.el = el
-    this.pri = pri
+let PriorityQueue = (function () {
+  /**
+   * 使用WeakMap，防止外界破坏，Object.getOwnPropertySymbols() 可获取对象中使用Symbol申明的属性
+   */
+  let items = new WeakMap()
+  /**
+   * 优先队列元素类
+   */
+  class QueueElement {
+    constructor(el, pri) {
+      this.el = el
+      this.pri = pri
+    }
   }
 
-  this.enqueue = function(el, pri){
-    let qe = new QueueElement(el, pri)
+  class PriorityQueue {
+    constructor(){
+      items.set(this, [])
+    }
+    /**
+     * 入队列
+     * @param {Object} el 当前节点数据源
+     * @param {Number} pri 当前节点数据源权重
+     */
+    enqueue(el, pri){
+      let qe = new QueueElement(el, pri)
 
-    let added = false
-    for (let i = 0;i < items.length;i++) {
-      if (qe.pri < items[i].pri) {
-        items.splice(i, 0, qe)
-        added = true
-        break
+      let added = false
+      let pqlist = items.get(this)
+      const len = pqlist.length
+      for (let i = 0; i < len; i++) {
+        if (qe.pri < pqlist[i].pri) {
+          pqlist.splice(i, 0, qe)
+          added = true
+          break
+        }
+      }
+      if (!added) {
+        pqlist.push(qe)
       }
     }
-    if (!added) {
-      items.push(qe)
+    /**
+     * 出队列
+     */
+    dequeue() {
+      let pqlist = items.get(this)
+      let r = pqlist.shift()
+      return r
+    }
+    /**
+     * 返回队列首元素
+     */
+    front() {
+      let q = items.get(this)
+      return q[0]
+    }
+    /**
+     * 队列是否为空
+     */
+    isEmpty() {
+      return items.get(this).length === 0
+    }
+    /**
+     * 队列长度
+     */
+    size() {
+      return items.get(this).length
+    }
+    /**
+     * 打印队列
+     */
+    print() {
+      let pqlist = items.get(this)
+      const len = pqlist.length
+      for (let i = 0; i < len; i++) {
+        console.log(`${pqlist[i].el}-${pqlist[i].pri}`)
+      }
     }
   }
-
-  this.print = function(){
-    for (let i = 0; i < items.length;i++) {
-      console.log(`${items[i].el}-${items[i].pri}`)
-    }
-  }
-}
+  return PriorityQueue
+}())
+export default PriorityQueue
--- Queue/Queue.js
+++ Queue/Queue.js
@@ -12,6 +12,19 @@ let Queue = (function(){
       let q = items.get(this)
       return q.shift()
     }
+    front(){
+      let q = items.get(this)
+      return q[0]
+    }
+    isEmpty() {
+      return items.get(this).length === 0
+    }
+    size() {
+      return items.get(this).length
+    }
+    print() {
+      console.log(items.get(this).toString())
+    }
   }
   return Queue
 }())
