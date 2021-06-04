c92161a214e20587f3d5e48b57325811ffa11b0e
huangwei <huangwei@ke.com>
2021-06-04 14:54:03 +0800

    带Promise 规范注释版本

--- Promise/Promise.js
+++ Promise/Promise.js
@@ -1,57 +1,224 @@
-const PENDING = 'PENDING'
-const FULFILLED = 'FULFILLED'
-const REJECTED = 'REJECTED'
+const PENDING = 'PENDING';
+const FULFILLED = 'FULFILLED';
+const REJECTED = 'REJECTED';
+
+const resolvePromise = (promise2, x, resolve, reject) => {
+  // 自己等待自己完成是错误的实现，用一个类型错误，结束掉 promise  Promise/A+ 2.3.1
+  if (promise2 === x) { 
+    return reject(new TypeError('Chaining cycle detected for promise #<Promise>'))
+  }
+  // Promise/A+ 2.3.3.3.3 只能调用一次
+  let called;
+  // 后续的条件要严格判断 保证代码能和别的库一起使用
+  if ((typeof x === 'object' && x != null) || typeof x === 'function') { 
+    try {
+      // 为了判断 resolve 过的就不用再 reject 了（比如 reject 和 resolve 同时调用的时候）  Promise/A+ 2.3.3.1
+      let then = x.then;
+      if (typeof then === 'function') { 
+        // 不要写成 x.then，直接 then.call 就可以了 因为 x.then 会再次取值，Object.defineProperty  Promise/A+ 2.3.3.3
+        then.call(x, y => { // 根据 promise 的状态决定是成功还是失败
+          if (called) return;
+          called = true;
+          // 递归解析的过程（因为可能 promise 中还有 promise） Promise/A+ 2.3.3.3.1
+          resolvePromise(promise2, y, resolve, reject); 
+        }, r => {
+          // 只要失败就失败 Promise/A+ 2.3.3.3.2
+          if (called) return;
+          called = true;
+          reject(r);
+        });
+      } else {
+        // 如果 x.then 是个普通值就直接返回 resolve 作为结果  Promise/A+ 2.3.3.4
+        resolve(x);
+      }
+    } catch (e) {
+      // Promise/A+ 2.3.3.2
+      if (called) return;
+      called = true;
+      reject(e)
+    }
+  } else {
+    // 如果 x 是个普通值就直接返回 resolve 作为结果  Promise/A+ 2.3.4  
+    resolve(x)
+  }
+}
 
 class Promise {
-  status = PENDING
-  value = undefined
-  reason = undefined
+  constructor(executor) {
+    this.status = PENDING;
+    this.value = undefined;
+    this.reason = undefined;
+    this.onResolvedCallbacks = [];
+    this.onRejectedCallbacks= [];
 
-  resolvedCallbacks = []
-  rejectedCallbacks = []
-
-  constructor(executor){
-    const resolve = (value) => {
-      if (this.status === PENDING) {
-        this.status = FULFILLED
-        this.value = value
-        this.resolvedCallbacks.forEach(cb => {
-          cb.apply(null, this.value)
-        })
+    let resolve = (value) => {
+      if(this.status ===  PENDING) {
+        this.status = FULFILLED;
+        this.value = value;
+        this.onResolvedCallbacks.forEach(fn=>fn());
       }
-    }
-    const reject = (reason) => {
-      if (this.status === PENDING) {
-        this.status = REJECTED
-        this.reason = reason
-        this.rejectedCallbacks.forEach(cb => {
-          cb.apply(null, this.reason)
-        })
+    } 
+
+    let reject = (reason) => {
+      if(this.status ===  PENDING) {
+        this.status = REJECTED;
+        this.reason = reason;
+        this.onRejectedCallbacks.forEach(fn=>fn());
       }
     }
 
     try {
-      executor(resolve, reject)
+      executor(resolve,reject)
     } catch (error) {
-     reject(error)
+      reject(error)
     }
   }
-
+  /**
+   * promise.then 链式调用
+   * @param {Function} onFulfilled
+   * @param {Function} onRejected
+   * @returns Promise
+   */
   then(onFulfilled, onRejected) {
-    if (this.status === FULFILLED) {
-      onFulfilled(this.value)
-    }
+    //解决 onFufilled，onRejected 没有传值的问题
+    //Promise/A+ 2.2.1 / Promise/A+ 2.2.5 / Promise/A+ 2.2.7.3 / Promise/A+ 2.2.7.4
+    onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : v => v;
+    //因为错误的值要让后面访问到，所以这里也要跑出个错误，不然会在之后 then 的 resolve 中捕获
+    onRejected = typeof onRejected === 'function' ? onRejected : err => { throw err };
+    // 每次调用 then 都返回一个新的 promise  Promise/A+ 2.2.7
+    const promise2 = new Promise((resolve, reject) => {
+      if (this.status === FULFILLED) {
+        //Promise/A+ 2.2.2
+        //Promise/A+ 2.2.4 --- setTimeout
+        setTimeout(() => {
+          try {
+            //Promise/A+ 2.2.7.1
+            const x = onFulfilled(this.value);
+            // x可能是一个proimise
+            resolvePromise(promise2, x, resolve, reject);
+          } catch (e) {
+            //Promise/A+ 2.2.7.2
+            reject(e)
+          }
+        }, 0);
+      }
 
-    if (this.status === REJECTED) {
-      onRejected(this.reason)
-    }
+      if (this.status === REJECTED) {
+        //Promise/A+ 2.2.3
+        setTimeout(() => {
+          try {
+            const x = onRejected(this.reason);
+            resolvePromise(promise2, x, resolve, reject);
+          } catch (e) {
+            reject(e)
+          }
+        }, 0);
+      }
 
-    if (this.status === PENDING) {
-      this.resolvedCallbacks.push(onFulfilled)
-      this.rejectedCallbacks.push(onRejected)
-    }
+      if (this.status === PENDING) {
+        this.onResolvedCallbacks.push(() => {
+          setTimeout(() => {
+            try {
+              const x = onFulfilled(this.value);
+              resolvePromise(promise2, x, resolve, reject);
+            } catch (e) {
+              reject(e)
+            }
+          }, 0);
+        });
+
+        this.onRejectedCallbacks.push(()=> {
+          setTimeout(() => {
+            try {
+              const x = onRejected(this.reason);
+              resolvePromise(promise2, x, resolve, reject)
+            } catch (e) {
+              reject(e)
+            }
+          }, 0);
+        });
+      }
+    });
+  
+    return promise2;
   }
 
+
+  catch(errCallback) {
+    return this.then(null,errCallback)
+  }
+
+  finally(callback) {
+    return this.then((value)=>{
+      return Promise.resolve(callback()).then(()=>value)
+    },(reason)=>{
+      return Promise.resolve(callback()).then(()=>{throw reason})
+    })  
+  }
+
+  static resolve(data){
+    return new Promise((resolve,reject)=>{
+      resolve(data);
+    })
+  }
+
+  static reject(reason){
+    return new Promise((resolve,reject)=>{
+      reject(reason);
+    })
+  } 
+
+  static all(values) {
+    if (!Array.isArray(values)) {
+      const type = typeof values;
+      return new TypeError(`TypeError: ${type} ${values} is not iterable`)
+    }
+
+    return new Promise((resolve, reject) => {
+      let resultArr = [];
+      let orderIndex = 0;
+      const processResultByKey = (value, index) => {
+        resultArr[index] = value;
+        if (++orderIndex === values.length) {
+            resolve(resultArr)
+        }
+      }
+      for (let i = 0; i < values.length; i++) {
+        let value = values[i];
+        if (value && typeof value.then === 'function') {
+          value.then((value) => {
+            processResultByKey(value, i);
+          }, reject);
+        } else {
+          processResultByKey(value, i);
+        }
+      }
+    });
+  }
+
+  static race(promises) {
+    return new Promise((resolve, reject) => {
+      for (let i = 0; i < promises.length; i++) {
+        let val = promises[i];
+        if (val && typeof val.then === 'function') {
+          val.then(resolve, reject);
+        } else {
+          resolve(val)
+        }
+      }
+    });
+  }
 }
 
-export default Promise
+
+
+Promise.defer = Promise.deferred = function () {
+  let dtd = {}
+  dtd.promise = new Promise((resolve, reject) => {
+    dtd.resolve = resolve;
+    dtd.reject = reject;
+  })
+  return dtd;
+}
+
+module.exports = Promise
